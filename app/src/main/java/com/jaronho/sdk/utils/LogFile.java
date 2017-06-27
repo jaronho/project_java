e8b469efca533db3c2bfae58e1366454f992481b
jaronho <892134825@qq.com>
2017-06-27 11:05:12 +0800

    modify LogFile

--- app/src/main/java/com/jaronho/sdk/utils/LogFile.java
+++ app/src/main/java/com/jaronho/sdk/utils/LogFile.java
@@ -22,7 +22,7 @@ import java.util.concurrent.ConcurrentLinkedQueue;
  */
 
 public class LogFile {
-	public static int FILE_MAX_SIZE = 1024 * 1024;  // 1M
+	private static long FILE_MAX_SIZE = 1024 * 1024;  // 1M
 	private static boolean mIsThreadRunning = false;
 	private static Thread mThread = new Thread(new Runnable() {
 		@Override
@@ -32,18 +32,24 @@ public class LogFile {
 				for (Map.Entry<String, ConcurrentLinkedQueue<String>> entry : entrySet) {
 					String fileFullPath = entry.getKey();
 					ConcurrentLinkedQueue<String> queue = entry.getValue();
+					long fileMaxSize = FILE_MAX_SIZE;
+					if (mSizeMap.containsKey(fileFullPath)) {
+						fileMaxSize = mSizeMap.get(fileFullPath);
+					}
 					while (null != queue && !queue.isEmpty()) {
 						String logString = queue.poll();
-						writeToFile(fileFullPath, logString);
+						writeToFile(fileFullPath, logString, fileMaxSize);
 					}
 				}
 			}
 		}
 	});
 	private static ConcurrentHashMap<String, ConcurrentLinkedQueue<String>> mLogMap = new ConcurrentHashMap<>();
+	private static ConcurrentHashMap<String, Long> mSizeMap = new ConcurrentHashMap<>();
 	private String mFileFullPath = "";
+	private long mFileMaxSize = FILE_MAX_SIZE;
 
-	private static void writeToFile(String fileFullPath, String logString) {
+	private static void writeToFile(String fileFullPath, String logString, long fileMaxSize) {
 		File file = new File(fileFullPath);
 		if (!file.exists()) {
 			if (file.getParentFile().exists() || file.getParentFile().mkdirs()) {
@@ -61,7 +67,7 @@ public class LogFile {
 		try {
 			long fileSize = file.length();
 			boolean isAppend = true;
-			if (fileSize >= FILE_MAX_SIZE) {
+			if (fileSize >= fileMaxSize) {
 				fileSize = 0;
 				isAppend = false;
 			}
@@ -81,8 +87,13 @@ public class LogFile {
 	@Override
 	protected void finalize() throws Throwable {
 		super.finalize();
-		if (!mFileFullPath.isEmpty() && mLogMap.containsKey(mFileFullPath)) {
-			mLogMap.remove(mFileFullPath);
+		if (!mFileFullPath.isEmpty()) {
+			if (mLogMap.containsKey(mFileFullPath)) {
+				mLogMap.remove(mFileFullPath);
+			}
+			if (mSizeMap.containsKey(mFileFullPath)) {
+				mSizeMap.remove(mFileFullPath);
+			}
 		}
 	}
 
@@ -90,12 +101,17 @@ public class LogFile {
 	 * 功  能: 构造函数
 	 * 参  数: context - 上下文活动
 	 * 		   filePath - 日志文件路径,默认为:外部存储路径+包名,例:"/sdcard/com.demo.app/"
-	 * 		   filePrefix - 日志文件名前缀,默认为"file"
+	 * 		   filePrefix - 日志文件名前缀,默认为"file",当filename不为空时,该值失效
+	 * 		   filename - 日志文件名,当该值不为空时,使用该值作为文件名,filePrefix失效
+	 * 		   fileMaxSize - 日志文件最大长度,当文件大小超过该值时,日志文件会被清空重新写入
 	 * 返回值: 无
 	 */
-	public LogFile(Context context, String filePath, String filePrefix) {
+	public LogFile(Context context, String filePath, String filePrefix, String filename, long fileMaxSize) {
 		if (null == filePath || filePath.isEmpty()) {
-			filePath = Environment.getExternalStorageDirectory().getPath() + "/" + context.getPackageName();
+			filePath = Environment.getExternalStorageDirectory().getPath();
+			if (null != context) {
+				filePath += "/" + context.getPackageName();
+			}
 		}
 		if (!filePath.endsWith("/")) {
 			filePath += "/";
@@ -105,9 +121,12 @@ public class LogFile {
 			if (Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED)) {
 				File dir = new File(filePath);
 				if (dir.exists() || dir.mkdirs()) {
-					String date = new SimpleDateFormat("yyyy-MM-dd-HH-mm-ss", Locale.getDefault()).format(new Date());
-					String filename = filePrefix + "-" + date + "-" + System.currentTimeMillis() + ".log";
+					if (null == filename || filename.isEmpty()) {
+						String date = new SimpleDateFormat("yyyy-MM-dd-HH-mm-ss", Locale.getDefault()).format(new Date());
+						filename = filePrefix + "-" + date + "-" + System.currentTimeMillis() + ".log";
+					}
 					mFileFullPath = filePath + filename;
+					mFileMaxSize = fileMaxSize > 0 ? fileMaxSize : FILE_MAX_SIZE;
 				}
 			}
 		} catch (Exception e) {
@@ -117,11 +136,21 @@ public class LogFile {
 
 	/**
 	 * 功  能: 构造函数
-	 * 参  数: 无
+	 * 参  数: context - 上下文活动
+	 *         fileMaxSize - 日志文件最大长度
+	 * 返回值: 无
+	 */
+	public LogFile(Context context, long fileMaxSize) {
+		this(context, "", "", "", fileMaxSize);
+	}
+
+	/**
+	 * 功  能: 构造函数
+	 * 参  数: context - 上下文活动
 	 * 返回值: 无
 	 */
 	public LogFile(Context context) {
-		this(context, "", "");
+		this(context, "", "", "", FILE_MAX_SIZE);
 	}
 
 	/**
@@ -151,6 +180,9 @@ public class LogFile {
 				queue = new ConcurrentLinkedQueue<>();
 				mLogMap.put(mFileFullPath, queue);
 			}
+			if (!mSizeMap.containsKey(mFileFullPath)) {
+				mSizeMap.put(mFileFullPath, mFileMaxSize);
+			}
 			queue.add(text);
 		}
 	}
@@ -174,6 +206,9 @@ public class LogFile {
 		if (!mFileFullPath.isEmpty()) {
 			if (mLogMap.containsKey(mFileFullPath)) {
 				mLogMap.remove(mFileFullPath);
+			}
+			if (mSizeMap.containsKey(mFileFullPath)) {
+				mSizeMap.remove(mFileFullPath);
 			}
 			File file = new File(mFileFullPath);
 			if (file.exists()){
